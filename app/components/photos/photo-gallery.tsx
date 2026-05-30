d0a8635033875bb0c912e45709f7eb9f2180391a
Hadarics Szabolcs <hasza98@gmail.com>
2026-05-30 08:58:16 +0200

    Added positioning to the images

--- app/components/photos/photo-gallery.tsx
+++ app/components/photos/photo-gallery.tsx
@@ -6,72 +6,83 @@ const photoModules = import.meta.glob("../../assets/photos/*.{jpg,jpeg,png,webp,
   import: "default",
 });
 
-const photoCaptions: Record<string, string> = {
-  "20221112_145907.jpg": "A prágai Lennon fal",
-  "20230324_033606.jpg": "D Lift",
-  "20230324_052702.jpg": "Repcsizünk Bariba",
-  "20230326_165456.jpg": "Alberobello és a Trullik",
-  "20240106_123323.jpg": "Éves városligeti korizás '24",
-  "20240502_194912.jpg": "Az Őrségi Patakparti Alpaka panzió",
-  "20240525_161100.jpg": "Kata & Frici - csokor elkapva ✅",
-  "20240627_012045.jpg": "BP Park HabParty után",
-  "20241205_191637.jpg": "Garden of Lights '24",
-  "20241228_144909.jpg": "Tihanyi apátság cukija",
-  "20250105_141141.jpg": "Alpe d'Huez '25 Kezdet",
-  "20250110_145848.jpg": "Alpe d'Huez '25 Vég",
-  "20250222_125520.jpg": "Éves városligeti korizás '25",
-  "20250418_171009.jpg": "Nagydorogi húsvéti dekorációban",
-  "20250517_084122.jpg": "Indul az olasz meló",
-  "20250531_121911.jpg": "Civitá - A Ponta Del Diavolo",
-  "20250531_150148.jpg": "Civitá - a mosolygós házak városa",
-  "20250531_172749.jpg": "Grotta del Saraceno - A fizetős insta strand",
-  "20250913_173912.jpg": "Trixi & Tuti",
-  "20250920_174753.jpg": "Jól sikerült családi vitorlázás",
-  "20251104_101233.jpg": "Felejthetetlen hely - az ujját nézzétek",
-  "20251105_170919.jpg": "Futóbolondok novmberben...",
-  "20260117_175607.jpg": "Garden of Lights '26",
-  "20260419_222225.jpg": "Aliz szülinapja (és a szomszédok) túlélve ✅",
-  "Aliz-Szatya-snowboardosnapok.jpg": "Chopok és Liptovszki Mikulás",
-  "att.FBsN3C7nGJ13wrrj2HjviJLCjVKYhN5ombrxTjfO0Qk.jpg":
-    "Jegyes fotózás a Margit-szigeten",
-  "att.gWgvAbjgxrQDtyZnYGhOGeYL2ORq0V95uudSwr1TDNg.jpg":
-    "Limoncello Spritz Tropeában",
-  "att.io1uPMN2vlxGJLAsTsPpFki_H7vV_IetKHw7KE97L0w.jpg":
-    "Jegyes fotózás, de itt már nagyon fáztunk",
-  "att.M8grU4WNj5e9TIZhdPMkyDrG9ehXIXxOd2HPmmZ8Dt4.jpg":
-    "A Sila-i óriásfenyők",
-  "att.tE2gYN4DrH7N4BIayS6H9XN4Jx_tJ9eGfcWneurAKCk.jpg":
-    "A legcukibb jegyesfotónk",
-  "att.wmXUvA4mExxIFH99y2vL5C3oef30NL4jzXUV-gsUN_c.jpg":
-    "Jegyefotó 3",
-  "att.zlJXcxRnND3lTq5ytC3uRbkV1cavRCsaLFNIdPecsIU.jpg":
-    "Aki ilyeneket szeretne, keresse Evelint! :)",
-  "IMG_0147.jpg": "Badacsonyi Bortúra",
-  "IMG_0576.jpg": "Garden of Lights '25",
-  "IMG_0588.jpg": "Bécsi karácsonyi vásár",
-  "IMG_1388.jpg": "A kedvenc Budaörsi IKEA",
-  "IMG_1504.jpg": "Alpe d'huez '25",
-  "IMG_1751.jpg": "Matera - a bírságok városa",
-  "IMG_20220816_204240_654.jpg": "Badacsonyi 2. évforduló",
-  "IMG_20220816_204240_681.jpg": "KZT '22",
-  "IMG_20220816_204240_698.jpg": "Sziszi & Erik",
-  "IMG_2310.jpg": "Az első esküvői helyszín amit megnéztünk - nem ez lett",
-  "IMG_2346.jpg": "Józsefvárosi Mikulásfutás",
-  "IMG_2442.jpg": "Dunai naplementés SUPozás",
-  "IMG_2710.jpg": "Túléltük Papa Sean koncertjét",
-  "received_651554755571652.jpeg": "Itt még nem tudták...",
+type PhotoPosition =
+  | "object-center"
+  | "object-top"
+  | "object-bottom"
+  | "object-left"
+  | "object-left-top"
+  | "object-left-bottom"
+  | "object-right"
+  | "object-right-top"
+  | "object-right-bottom";
+
+type PhotoMetadata = {
+  caption: string;
+  position: PhotoPosition;
+};
+
+const photoMetadata: Record<string, PhotoMetadata> = {
+  "20221112_145907.jpg": { caption: "A prágai Lennon fal", position: "object-center" },
+  "20230324_033606.jpg": { caption: "D Lift", position: "object-right-top" },
+  "20230324_052702.jpg": { caption: "Repcsizünk Bariba", position: "object-top" },
+  "20230326_165456.jpg": { caption: "Alberobello és a Trullik", position: "object-left" },
+  "20240106_123323.jpg": { caption: "Éves városligeti korizás '24", position: "object-center" },
+  "20240502_194912.jpg": { caption: "Az Őrségi Patakparti Alpaka panzió", position: "object-center" },
+  "20240525_161100.jpg": { caption: "Kata & Frici - csokor elkapva ✅", position: "object-center" },
+  "20240627_012045.jpg": { caption: "BP Park HabParty után", position: "object-center" },
+  "20241205_191637.jpg": { caption: "Garden of Lights '24", position: "object-center" },
+  "20241228_144909.jpg": { caption: "Tihanyi apátság cukija", position: "object-center" },
+  "20250105_141141.jpg": { caption: "Alpe d'Huez '25 Kezdet", position: "object-center" },
+  "20250110_145848.jpg": { caption: "Alpe d'Huez '25 Vég", position: "object-top" },
+  "20250222_125520.jpg": { caption: "Éves városligeti korizás '25", position: "object-center" },
+  "20250418_171009.jpg": { caption: "Nagydorogi húsvéti dekorációban", position: "object-center" },
+  "20250517_084122.jpg": { caption: "Indul az olasz meló", position: "object-center" },
+  "20250531_121911.jpg": { caption: "Civitá - A Ponta Del Diavolo", position: "object-center" },
+  "20250531_150148.jpg": { caption: "Civitá - a mosolygós házak városa", position: "object-top" },
+  "20250531_172749.jpg": { caption: "Grotta del Saraceno - A fizetős insta strand", position: "object-center" },
+  "20250913_173912.jpg": { caption: "Trixi & Tuti", position: "object-center" },
+  "20250920_174753.jpg": { caption: "Jól sikerült családi vitorlázás", position: "object-center" },
+  "20251104_101233.jpg": { caption: "Felejthetetlen hely - az ujját nézzétek", position: "object-center" },
+  "20251105_170919.jpg": { caption: "Futóbolondok novmberben...", position: "object-center" },
+  "20260117_175607.jpg": { caption: "Garden of Lights '26", position: "object-center" },
+  "20260419_222225.jpg": { caption: "Aliz szülinapja (és a szomszédok) túlélve ✅", position: "object-center" },
+  "Aliz-Szatya-snowboardosnapok.jpg": { caption: "Chopok és Liptovszki Mikulás", position: "object-center" },
+  "att.FBsN3C7nGJ13wrrj2HjviJLCjVKYhN5ombrxTjfO0Qk.jpg": { caption: "Jegyes fotózás a Margit-szigeten", position: "object-center" },
+  "att.gWgvAbjgxrQDtyZnYGhOGeYL2ORq0V95uudSwr1TDNg.jpg": { caption: "Limoncello Spritz Tropeában", position: "object-center" },
+  "att.io1uPMN2vlxGJLAsTsPpFki_H7vV_IetKHw7KE97L0w.jpg": { caption: "Jegyes fotózás, de itt már nagyon fáztunk", position: "object-center" },
+  "att.M8grU4WNj5e9TIZhdPMkyDrG9ehXIXxOd2HPmmZ8Dt4.jpg": { caption: "A Sila-i óriásfenyők", position: "object-center" },
+  "att.tE2gYN4DrH7N4BIayS6H9XN4Jx_tJ9eGfcWneurAKCk.jpg": { caption: "A legcukibb jegyesfotónk", position: "object-center" },
+  "att.wmXUvA4mExxIFH99y2vL5C3oef30NL4jzXUV-gsUN_c.jpg": { caption: "Jegyefotó 3", position: "object-center" },
+  "att.zlJXcxRnND3lTq5ytC3uRbkV1cavRCsaLFNIdPecsIU.jpg": { caption: "Aki ilyeneket szeretne, keresse Evelint! :)", position: "object-center" },
+  "IMG_0147.jpg": { caption: "Badacsonyi Bortúra", position: "object-center" },
+  "IMG_0576.jpg": { caption: "Garden of Lights '25", position: "object-center" },
+  "IMG_0588.jpg": { caption: "Bécsi karácsonyi vásár", position: "object-center" },
+  "IMG_1388.jpg": { caption: "A kedvenc Budaörsi IKEA", position: "object-center" },
+  "IMG_1504.jpg": { caption: "Alpe d'huez '25", position: "object-center" },
+  "IMG_1751.jpg": { caption: "Matera - a bírságok városa", position: "object-center" },
+  "IMG_20220816_204240_654.jpg": { caption: "Badacsonyi 2. évforduló", position: "object-center" },
+  "IMG_20220816_204240_681.jpg": { caption: "KZT '22", position: "object-center" },
+  "IMG_20220816_204240_698.jpg": { caption: "Sziszi & Erik", position: "object-center" },
+  "IMG_2310.jpg": { caption: "Az első esküvői helyszín amit megnéztünk - nem ez lett", position: "object-center" },
+  "IMG_2346.jpg": { caption: "Józsefvárosi Mikulásfutás", position: "object-center" },
+  "IMG_2442.jpg": { caption: "Dunai naplementés SUPozás", position: "object-center" },
+  "IMG_2710.jpg": { caption: "Túléltük Papa Sean koncertjét", position: "object-center" },
+  "received_651554755571652.jpeg": { caption: "Itt még nem tudták...", position: "object-center" },
 };
 
 const photos = Object.entries(photoModules)
   .map(([path, src]) => {
     const fileName = path.split("/").pop() ?? "";
+    const metadata = photoMetadata[fileName];
 
     return {
       src: src as string,
       alt:
         fileName.replace(/\.[^.]+$/, "").replace(/[-_]/g, " ") ??
         "Wedding photo",
-      caption: photoCaptions[fileName] ?? "",
+      caption: metadata?.caption ?? "",
+      position: metadata?.position ?? "object-center",
     };
   })
   .sort((a, b) => a.alt.localeCompare(b.alt));
@@ -176,7 +187,9 @@ function PhotoCard({
         src={photo.src}
         alt={photo.alt}
         loading="lazy"
-        className="aspect-square w-full object-cover"
+        className={["aspect-square w-full object-cover", photo.position].join(
+          " ",
+        )}
       />
       {photo.caption && (
         <figcaption
